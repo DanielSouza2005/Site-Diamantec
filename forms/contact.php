255601b9cb7b614d284cbad48d558b3a3d7e13aa
DanielSouza2005 <danisimoes741@gmail.com>
2023-01-04 11:40:59 -0300

    teste

--- forms/contact.php
+++ forms/contact.php
@@ -12,7 +12,7 @@
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die( 'Sua mensagem foi enviada com sucesso!');
   }
 
   $contact = new PHP_Email_Form;
